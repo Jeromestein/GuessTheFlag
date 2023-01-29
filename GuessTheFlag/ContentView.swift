d09c8057bb126907074ef766dd0f3a49c9b3e7d6
Jerome <nietsemorej@gmail.com>
2023-01-28 20:23:36 -0500

    Stacking up buttons and showing the player’s score with an alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+        .shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var scoreTitle = ""
+    @State private var showingScore = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack{
+                Text("Tap The Flag Of")
+                Text(countries[correctAnswer])
+            }
+            
+            ForEach(0..<3) { number in
+                Button {
+                    flagTapped(number: number)
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                }
+            }
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your Score is ???")
+        }
+        
+        
+    }
+    
+    func flagTapped(number: Int)  {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
